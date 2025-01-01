a8531723c18e91dc43d1a741d27f257a60a62c99
chnoumanejaz <noumanejaz92@gmail.com>
2025-01-01 23:49:23 +0500

    enhance chat and list components with search functionality

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,57 @@
-import "./chatList.css"
+import { useState } from 'react';
+import { FaMinus, FaSearch } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa6';
+import './chatList.css';
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div className="chatList">ChatList</div>
-  )
-}
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <FaSearch />
+          <input type="text" placeholder="Search by user name" />
+        </div>
 
-export default ChatList
+        <div onClick={() => setAddMode(!addMode)} className="svg">
+          {addMode ? <FaMinus /> : <FaPlus />}
+        </div>
+      </div>
+
+      <div className="item">
+        {/* TODO: change the alt attribute value */}
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>User name</span>
+          <p>hello hiiii</p>
+        </div>
+      </div>
+      <div className="item">
+        {/* TODO: change the alt attribute value */}
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>User name</span>
+          <p>hello hiiii</p>
+        </div>
+      </div>
+      <div className="item">
+        {/* TODO: change the alt attribute value */}
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>User name</span>
+          <p>hello hiiii</p>
+        </div>
+      </div>
+      <div className="item">
+        {/* TODO: change the alt attribute value */}
+        <img src="./avatar.png" alt="user" />
+        <div className="texts">
+          <span>User name</span>
+          <p>hello hiiii</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
