1abe801527ff01806c734b514ef732fe8bb834d6
chnoumanejaz <noumanejaz92@gmail.com>
2025-01-03 13:35:26 +0500

    implement real-time chat updates and user search functionality

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import { FaMinus, FaSearch } from 'react-icons/fa';
 import { FaPlus } from 'react-icons/fa6';
-import './chatList.css';
+import { db } from '../../../lib/firebase';
+import { useUserStore } from '../../../lib/userStore';
 import AddUser from './addUser/AddUser';
+import './chatList.css';
 
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onSnapshot(
+      doc(db, 'userchats', currentUser.id),
+      async res => {
+        const items = res.data().chats;
+
+        const promisses = items.map(async item => {
+          const userDoc = await getDoc(doc(db, 'users', item.receiverId));
+          const user = userDoc.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promisses);
+
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
+
   return (
     <div className="chatList">
       <div className="search">
@@ -19,38 +49,23 @@ const ChatList = () => {
         </div>
       </div>
 
-      <div className="item">
-        {/* TODO: change the alt attribute value */}
-        <img src="./avatar.png" alt="user" />
-        <div className="texts">
-          <span>User name</span>
-          <p>hello hiiii</p>
-        </div>
-      </div>
-      <div className="item">
-        {/* TODO: change the alt attribute value */}
-        <img src="./avatar.png" alt="user" />
-        <div className="texts">
-          <span>User name</span>
-          <p>hello hiiii</p>
-        </div>
-      </div>
-      <div className="item">
-        {/* TODO: change the alt attribute value */}
-        <img src="./avatar.png" alt="user" />
-        <div className="texts">
-          <span>User name</span>
-          <p>hello hiiii</p>
-        </div>
-      </div>
-      <div className="item">
-        {/* TODO: change the alt attribute value */}
-        <img src="./avatar.png" alt="user" />
-        <div className="texts">
-          <span>User name</span>
-          <p>hello hiiii</p>
-        </div>
-      </div>
+      {chats.length > 0 ? (
+        chats.map(chat => (
+          <div className="item" key={chat.chatId}>
+            <img src="./avatar.png" alt={`${chat.user.username}'s avatar`} />
+            <div className="texts">
+              <span>{chat.user.name}</span>
+              <p>{chat.lastMessage}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h3 className="noChatMsg">
+          No chats available. Start new chat by clicking on the plus button
+          above.
+        </h3>
+      )}
+
       {addMode && <AddUser />}
     </div>
   );
--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,30 +1,131 @@
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { db } from '../../../../lib/firebase';
+import { useUserStore } from '../../../../lib/userStore';
 import './addUser.css';
 
 const AddUser = () => {
+  const [searchedUser, setSearchedUser] = useState(null);
+  const [searchCount, setSearchCount] = useState(0);
+  const [isSearching, setIsSearching] = useState(false);
+  const { currentUser } = useUserStore();
+  const [quickAddUsers, setQuickAddUsers] = useState([]);
+  const [isLoadingQuickUsers, setIsLoadingQuickUsers] = useState(false);
+
+  useEffect(() => {
+    const fetchQuickUsers = async () => {
+      setIsLoadingQuickUsers(true);
+      try {
+        const userRef = collection(db, 'users');
+        const q = query(userRef, limit(4));
+        const querySnapshot = await getDocs(q);
+
+        const fetchedUsers = [];
+        querySnapshot.forEach(doc => {
+          fetchedUsers.push({
+            ...doc.data(),
+          });
+        });
+
+        setQuickAddUsers(fetchedUsers);
+      } catch (error) {
+        console.error('Error fetching quick users: ', error);
+      } finally {
+        setIsLoadingQuickUsers(false);
+      }
+    };
+
+    fetchQuickUsers();
+  }, []);
+
+  const handleSearch = async e => {
+    e.preventDefault();
+    setIsSearching(true);
+    const formData = new FormData(e.target);
+    const username = formData.get('username');
+
+    if (!username) {
+      setIsSearching(false);
+      toast.error('Please enter a username');
+      return;
+    }
+
+    try {
+      setSearchCount(searchCount + 1);
+      const userRef = collection(db, 'users');
+      const q = query(userRef, where('username', '==', username));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        setSearchedUser(querySnapshot.docs[0].data());
+      } else {
+        setSearchedUser(null);
+      }
+    } catch (error) {
+      console.error('Failed to search user', error);
+    } finally {
+      setIsSearching(false);
+    }
+  };
+
   return (
     <div className="addUser">
-      <form>
-        <input type="text" placeholder="username" name="username" />
-        <button>Search</button>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Enter username"
+          name="username"
+          disabled={isSearching}
+          autoFocus
+        />
+        <button disabled={isSearching}>
+          {isSearching ? 'Searching...' : 'Search'}
+        </button>
       </form>
 
       <div className="usersContainer">
-        <div className="user">
-          <div className="detail">
-            {/* TODO: Add an alt attribute to the image below */}
-            <img src="./avatar.png" alt="username" />
-            <span>User name</span>
+        {searchedUser && (
+          <div className="user">
+            <div className="detail">
+              <img
+                src={searchedUser.avatar || './avatar.png'}
+                alt={searchedUser.username + "'s avatar"}
+              />
+              <span>
+                {searchedUser.username === currentUser.username
+                  ? searchedUser.username + ' (You)'
+                  : searchedUser.username}
+              </span>
+            </div>
+            <button disabled={isSearching}>Add User</button>
           </div>
-          <button>Add User</button>
-        </div>
-        <div className="user">
-          <div className="detail">
-            {/* TODO: Add an alt attribute to the image below */}
-            <img src="./avatar.png" alt="username" />
-            <span>User name</span>
-          </div>
-          <button>Add User</button>
-        </div>
+        )}
+
+        {!searchedUser && searchCount > 0 && !isSearching && (
+          <>
+            <h4>No user found with that username</h4>
+          </>
+        )}
+
+        {searchCount == 0 &&
+          quickAddUsers.map(user => {
+            if (currentUser.username === user.username) return null;
+            return (
+              <div className="user" key={user.id}>
+                <div className="detail">
+                  <img
+                    src={user.avatar || './avatar.png'}
+                    alt={user.username + "'s avatar"}
+                  />
+                  <span>{user.username}</span>
+                </div>
+                <button disabled={isSearching}>Add User</button>
+              </div>
+            );
+          })}
+
+        {isLoadingQuickUsers && <h4>Loading quick add users...</h4>}
       </div>
     </div>
   );
